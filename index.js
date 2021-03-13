e365b65ed00ec80a3da057733782f03f93102918
security-notifications-center <80545980+security-notifications-center@users.noreply.github.com>
2021-03-12 22:34:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const viewLog = []
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/ping', (req, res) => {
+    viewLog.push(JSON.stringify(req))
+    res.end(viewLog)
+  })
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
